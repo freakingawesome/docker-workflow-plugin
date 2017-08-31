8c29a105195cf3bba50f96aef3bbc0f0699f00c6
Chad Gilbert <chad.gilbert@cqlcorp.com>
2017-08-31 07:58:43 -0400

    Avoid unsupported bind mount option of z on Windows containers

--- src/main/java/org/jenkinsci/plugins/docker/workflow/client/DockerClient.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/client/DockerClient.java
@@ -120,7 +120,7 @@ public class DockerClient {
             argb.add("-w", workdir);
         }
         for (Map.Entry<String, String> volume : volumes.entrySet()) {
-            argb.add("-v", volume.getKey() + ":" + volume.getValue() + ":rw,z");
+            argb.add("-v", volume.getKey() + ":" + volume.getValue() + (launcher.isUnix() ? ":rw,z": ":rw"));
         }
         for (String containerId : volumesFromContainers) {
             argb.add("--volumes-from", containerId);
